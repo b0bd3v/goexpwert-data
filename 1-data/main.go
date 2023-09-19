5a50bbd2f01a9e3fcb7eed4db4e5b4f674549a03
Roberto Martins <roberto.martins.info@gmail.com>
2023-09-19 20:38:57 -0300

    server

---
+++ 1-data/main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/uuid"
+)
+
+type Product struct {
+	ID    string
+	Name  string
+	Price float64
+}
+
+func NewProduct(name string, price float64) *Product {
+	return &Product{
+		ID:    uuid.New().String(),
+		Name:  name,
+		Price: price,
+	}
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(localhost:33062)/goexpert")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer db.Close()
+
+	product := NewProduct("Product 1", 1000)
+
+	err = insertProduct(db, product)
+	product.Price = 1021.12
+	err = updateProduct(db, product)
+
+	if err != nil {
+		panic(err)
+	}
+
+	p, err := selectOneProduct(db, product.ID)
+	fmt.Printf("Product %v, possui o prećo de %.2f", p.Name, p.Price)
+}
+
+func insertProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("INSERT INTO products(id, name, price) VALUES(?, ?, ?)")
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.ID, product.Name, product.Price)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func updateProduct(db *sql.DB, product *Product) error {
+	stmt, err := db.Prepare("UPDATE products SET name = ?, price = ? WHERE id = ?")
+
+	if err != nil {
+		return err
+	}
+
+	defer stmt.Close()
+
+	_, err = stmt.Exec(product.Name, product.Price, product.ID)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func selectOneProduct(db *sql.DB, id string) (*Product, error) {
+	stmt, err := db.Prepare("SELECT id, name, price FROM products WHERE id = ?")
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer stmt.Close()
+
+	var product Product
+
+	err = stmt.QueryRow(id).Scan(&product.ID, &product.Name, &product.Price)
+
+	if err != nil {
+		panic(err)
+	}
+
+	return &product, nil
+}
